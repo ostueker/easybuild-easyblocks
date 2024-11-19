b3031dd10072244a5babd42fe024d0e131a20763
Bart Oldeman <bart.oldeman@calculquebec.ca>
2024-11-19 15:14:12 +0000

    Add comment about modifyMesh removal

--- easybuild/easyblocks/o/openfoam.py
+++ easybuild/easyblocks/o/openfoam.py
@@ -487,6 +487,7 @@ class EB_OpenFOAM(EasyBlock):
             if self.looseversion < LooseVersion("11"):
                 tools.append("buoyantFoam")
                 tools.append("reactingFoam")
+        # modifyMesh is no longer there in OpenFOAM >= 12
         if self.is_dot_org and self.looseversion >= LooseVersion("12"):
             tools.remove("modifyMesh")
         if self.looseversion >= LooseVersion('2406'):
