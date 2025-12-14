e22fc2a50a1af98f338c0ee01bc7035f793e6c24
Kenneth Hoste <kenneth.hoste@ugent.be>
2025-12-14 11:21:39 +0100

    also disable core binding in test step of FFTW easyblock, required when building with NVHPC, see also https://github.com/open-mpi/ompi/issues/12470

--- easybuild/easyblocks/f/fftw.py
+++ easybuild/easyblocks/f/fftw.py
@@ -243,11 +243,14 @@ class EB_FFTW(ConfigureMake):
     def test_step(self):
         """Custom implementation of test step for FFTW."""
 
-        if self.toolchain.mpi_family() is not None and not build_option('mpi_tests'):
+        comp_family = self.toolchain.comp_family()
+        mpi_family = self.toolchain.mpi_family()
+
+        if mpi_family is not None and not build_option('mpi_tests'):
             self.log.info("Skipping testing of FFTW since MPI testing is disabled")
             return
 
-        if self.toolchain.mpi_family() == toolchain.OPENMPI and not self.toolchain.comp_family() == TC_CONSTANT_FUJITSU:
+        if mpi_family == toolchain.OPENMPI and comp_family != TC_CONSTANT_FUJITSU:
 
             # allow oversubscription of number of processes over number of available cores with OpenMPI 3.0 & newer,
             # to avoid that some tests fail if only a handful of cores are available
@@ -258,6 +261,12 @@ class EB_FFTW(ConfigureMake):
             if ompi_ver and LooseVersion(ompi_ver) >= LooseVersion('5.0'):
                 if 'PRTE_MCA_rmaps_default_mapping_policy' not in self.cfg['pretestopts']:
                     self.cfg.update('pretestopts', "export PRTE_MCA_rmaps_default_mapping_policy=:oversubscribe && ")
+
+                # workaround for problem with core binding with OpenMPI 4.x,
+                # errors like: hwloc_set_cpubind returned "Error" for bitmap "0"
+                # see https://github.com/open-mpi/ompi/issues/12470
+                if 'OMPI_MCA_hwloc_base_binding_policy' not in self.cfg['pretestopts']:
+                    self.cfg.update('pretestopts', "export OMPI_MCA_hwloc_base_binding_policy=none && ")
 
         super().test_step()
 
