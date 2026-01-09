47d6c56956ee1343144d3b04362ffa01d3e75bdf
Bart Oldeman <bart.oldeman@calculquebec.ca>
2026-01-09 12:24:11 +0000

    Sync fftw with upstream

--- easybuild/easyblocks/f/fftw.py
+++ easybuild/easyblocks/f/fftw.py
@@ -255,12 +255,13 @@ class EB_FFTW(ConfigureMake):
             # allow oversubscription of number of processes over number of available cores with OpenMPI 3.0 & newer,
             # to avoid that some tests fail if only a handful of cores are available
             ompi_ver = get_software_version('OpenMPI')
-            if ompi_ver and LooseVersion(ompi_ver) >= LooseVersion('3.0') and LooseVersion(ompi_ver) < LooseVersion('5.0'):
-                if 'OMPI_MCA_rmaps_base_oversubscribe' not in self.cfg['pretestopts']:
-                    self.cfg.update('pretestopts', "export OMPI_MCA_rmaps_base_oversubscribe=true && ")
             if ompi_ver and LooseVersion(ompi_ver) >= LooseVersion('5.0'):
                 if 'PRTE_MCA_rmaps_default_mapping_policy' not in self.cfg['pretestopts']:
                     self.cfg.update('pretestopts', "export PRTE_MCA_rmaps_default_mapping_policy=:oversubscribe && ")
+            else:
+                # OpenMPI 4 and older (including NVHPC)
+                if 'OMPI_MCA_rmaps_base_oversubscribe' not in self.cfg['pretestopts']:
+                    self.cfg.update('pretestopts', "export OMPI_MCA_rmaps_base_oversubscribe=true && ")
 
                 # workaround for problem with core binding with OpenMPI 4.x,
                 # errors like: hwloc_set_cpubind returned "Error" for bitmap "0"
