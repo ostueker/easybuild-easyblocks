e7493508857e2cd6a00de43131e89931d410fa20
Alexander Grund <alexander.grund@tu-dresden.de>
2026-01-22 10:54:49 +0100

    Fix sanity-check-only

--- easybuild/easyblocks/t/tensorrt.py
+++ easybuild/easyblocks/t/tensorrt.py
@@ -99,12 +99,13 @@ class EB_TensorRT(PythonPackage, Binary):
     def sanity_check_step(self):
         """Custom sanity check for TensorRT."""
         custom_paths = {
-            'dirs': [self.pylibdir],
+            'dirs': [os.path.join('lib', 'python%(pyshortver)s', 'site-packages')],
         }
         if LooseVersion(self.version) >= LooseVersion('6'):
-            custom_paths['files'] = ['bin/trtexec', 'lib/libnvinfer_static.a']
+            lib_name = 'libnvinfer_static.a'
         else:
-            custom_paths['files'] = ['bin/trtexec', 'lib/libnvinfer.a']
+            lib_name = 'libnvinfer.a'
+        custom_paths = {'files': ['bin/trtexec', f'lib/{lib_name}']}
 
         custom_commands = [
             "python -c 'import tensorrt'",
